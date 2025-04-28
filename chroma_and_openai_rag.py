0d9d1361af94bb83b5e08047857f937a45fdf571
vrige <manuel.96524@gmail.com>
2025-04-28 14:06:41 +0200

    Creating an api with custom web page rag integrated.

--- chroma_and_openai_rag.py
+++ chroma_and_openai_rag.py
@@ -6,29 +6,28 @@ from chroma.utils import ChromaUtils
 from openai_utils.openai_utils import OpenaiAgent
 
 chroma_client = ChromaUtils()
-chroma_client.sync_create_coll(collection_name="sanremo_rag")
+chroma_client.sync_create_coll(collection_name="your_coll_name")
 
 app = Flask(__name__)
 
 
-def store_data():
+def store_data(url: str) -> None:
 
     # Step 1 - Load documents from a web page
-    loader = WebBaseLoader("https://it.wikipedia.org/wiki/Festival_di_Sanremo#Edizioni")
+    loader = WebBaseLoader(web_path=url)
     docs = loader.load()
-    #document_text = " ".join([doc.page_content for doc in docs])
+    if docs:
+        # Step 2 - Creating embedding and splitter
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        documents = text_splitter.split_documents(docs)
+        documents_text = [doc.page_content for doc in documents]
 
+        # Step 3 - Store the data
+        ids = chroma_client.sync_add_vectors(
+            collection_name="your_coll_name",
+            doc_list=documents_text
+        )
 
-    # Step 2 - Creating embedding and splitter
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    documents = text_splitter.split_documents(docs)
-    documents_text = [doc.page_content for doc in documents]
-
-    # Step 3 - Store the data
-    ids = chroma_client.sync_add_vectors(
-        collection_name="sanremo_rag",
-        doc_list=documents_text
-    )
 
 @app.route('/conversate', methods=['POST'])
 def main():
@@ -38,12 +37,15 @@ def main():
     if not query:
         return jsonify({"output":"Error: please provide a \'query\' field."})
 
+    if "url" in input_json:
+        store_data(input_json["url"])
+
     # Step 4 - Create Agent
     agent = OpenaiAgent(model="gpt-4o")
 
     # Step 5 - Retrieve
     results = chroma_client.sync_query_collection(
-        collection_name="sanremo_rag",
+        collection_name="your_coll_name",
         query = query
     )
     context = "\n".join(results["documents"][0])
